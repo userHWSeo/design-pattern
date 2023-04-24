a6b9702bb4ef384aa296c8f31753bfd925e3eacc
useHWSeo <gmldnjseocnd@naver.com>
2023-04-24 21:00:52 +0900

    전략 패턴 추가
    - 전략 패턴 예시 추가와 팩토리패턴의 차이점

---
+++ js-design-pattern/strategy.js
@@ -0,0 +1,14 @@
+const shoesBrand = (arr) => {
+  console.log(`${arr[0]} 브랜드의 ${arr[1]} 제품을 원하시는군요 !`);
+  console.log(`좋은 하루 되세요 ~`);
+};
+
+const nikeShoes = () => {
+  console.log("이건 세일 중이에요 !");
+  return ["Nike", "Jordan"];
+};
+
+const vansShoes = () => {
+  console.log("260 사이즈는 품절이에요 ㅠ");
+  return ["Vans", "Old School"];
+};
